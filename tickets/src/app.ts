619b9195194b40193248e57b3f7ce0cd53da722d
shrinidhi Karanth <shrinidhiskaranth@gmail.com>
2025-10-18 14:24:05 +0530

    Added Test Cases for ticket service

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -3,10 +3,11 @@ import { json } from 'body-parser';
 import mongoose from 'mongoose';
 import { createTicketRouter } from './routes/new';
 import { showTicketRouter } from './routes/show';
-
+import { indexRouter } from './routes/index';
 import { errorHandler, currentUser } from '@retix/common';
 import { NotFoundError } from '@retix/common';
 import cookieSession from 'cookie-session';
+import { updateTicketRouter } from './routes/update';
 
 const app = express();
 app.set('trust proxy', true);
@@ -19,6 +20,8 @@ app.use(cookieSession({
 app.use(currentUser);
 app.use(createTicketRouter);
 app.use(showTicketRouter);
+app.use(indexRouter);
+app.use(updateTicketRouter);
 
 app.use((req, res, next) => {
     next(new NotFoundError());
---
+++ tickets/src/routes/__test__/update.test.ts
@@ -0,0 +1,87 @@
+import request from 'supertest';
+import { app } from "../../app";
+import mongoose from 'mongoose';
+import { Ticket } from "../../models/ticket";
+
+it('returns a 404 if the ticket is not found', async () => {
+    const id = new mongoose.Types.ObjectId().toHexString();
+    await request(app).put(`/api/tickets/${id}`)
+    .set('Cookie', global.signin())
+    .send({
+        title: 'new title',
+        price: 10
+    }).expect(404)
+})
+
+it("returns a 401 if the user is not authenticated", async () => {
+   const id  = new mongoose.Types.ObjectId().toHexString();
+   await request(app).put(`/api/tickets/${id}`)
+   .send({
+       title: 'new title',
+       price: 10
+   }).expect(401)
+})
+
+it("returs 401 if user does not own the ticket", async () => {
+    const ticket = await request(app).post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+        title: 'new title',
+        price: 10
+    })
+    const ticketId = ticket.body.id;
+    await request(app).put(`/api/tickets/${ticketId}`)
+    .set('Cookie', global.signin())
+    .send({
+        title: 'new title',
+        price: 10
+    }).expect(401)
+})
+
+it("returns a 400 if the user provides an invalid title or price", async () => {
+    const cookie = global.signin()
+    const ticket = await request(app).post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+        title: 'new title',
+        price: 10
+    })
+    const ticketId = ticket.body.id;
+    await request(app).put(`/api/tickets/${ticketId}`)
+    .set('Cookie', cookie)
+    .send({
+        price: -10
+    }).expect(400)
+
+    await request(app).put(`/api/tickets/${ticketId}`)
+    .set('Cookie', cookie)
+    .send({
+        title: '',
+        price: 10
+    }).expect(400)
+})
+
+it("updates the ticket provided valid inputs", async () => {
+    const cookie = global.signin()
+    const ticket = await request(app).post('/api/tickets')
+    .set('Cookie', cookie)
+    .send({
+        title: '    new title',
+        price: 10
+    })
+    const ticketId = ticket.body.id;
+    await request(app).put(`/api/tickets/${ticketId}`)
+    .set('Cookie', cookie)
+    .send({
+        title: 'updated title',
+        price: 100
+    }).expect(200)
+
+    const updatedTicket = await request(app).get(`/api/tickets/${ticketId}`)
+    .set('Cookie', cookie)
+    .send()
+    expect(updatedTicket.body.title).toEqual('updated title')
+    expect(updatedTicket.body.price).toEqual(100)
+})
+
+   
