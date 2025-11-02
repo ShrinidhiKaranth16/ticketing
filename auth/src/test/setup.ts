d6b299b0a5a08bc8ccd677d883d66d4dae17758f
ShrinidhiKaranth16 <56083767+ShrinidhiKaranth16@users.noreply.github.com>
2025-11-02 16:25:29 +0530

    Version 7.0.5

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -16,7 +16,7 @@ beforeAll(async () => {
   process.env.JWT_KEY = 'asdf';
 
   mongo = await MongoMemoryServer.create({
-   binary: { version: '5.0.15' },
+   binary: { version: '7.0.5' },
     instance: {
       dbName: 'test' // Optional: add database name
     }
