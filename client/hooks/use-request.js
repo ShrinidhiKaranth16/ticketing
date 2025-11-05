10cb066f2b136b7a576eaee641195dd3af771186
ShrinidhiKaranth16 <56083767+ShrinidhiKaranth16@users.noreply.github.com>
2025-11-05 10:57:17 +0530

    Fixing oops error

--- client/hooks/use-request.js
+++ client/hooks/use-request.js
@@ -1,31 +1,38 @@
 import axios from "axios";
 import { useState } from "react";
 
-const useRequest = ({ url, method, body,onSuccess }) => {
+const useRequest = ({ url, method, body, onSuccess }) => {
     const [errors, setErrors] = useState(null);
-    const doRequest = async (props={}) => {
+    const doRequest = async (props = {}) => {
         try {
             setErrors(null);
             const response = await axios[method](url,
-            {
-                ...body,
-                ...props
-            }
+                {
+                    ...body,
+                    ...props
+                }
             );
-            if(onSuccess){
+            if (onSuccess) {
                 onSuccess(response.data);
-            }            
+            }
             return response.data;
         } catch (err) {
             setErrors(
                 <div className="alert alert-danger" role="alert">
                     <h4>Oooops!</h4>
                     <ul className="my-0">
-                        {err.response.data.errors.map((error) => (
-                            <li className="text-danger" key={error}>
-                                {error.message}
+                        {err?.response?.data?.errors ? (
+                            err.response.data.errors.map((error, index) => (
+                                <li className="text-danger" key={index}>
+                                    {error.message}
+                                </li>
+                            ))
+                        ) : (
+                            <li className="text-danger">
+                                {err?.message || 'Something went wrong. Please try again.'}
                             </li>
-                        ))}
+                        )}
+
                     </ul>
                 </div>
             );
