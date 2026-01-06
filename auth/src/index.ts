052302a680cdfe7f2db29c5dbc80d35707980de1
ShrinidhiKaranth16 <56083767+ShrinidhiKaranth16@users.noreply.github.com>
2026-01-06 13:37:31 +0530

    testing auth

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,7 +5,7 @@ import { app } from './app';
 const start = async () => {
     console.log('Starting auth!!!! 1');
     if(!process.env.JWT_KEY){
-        throw new Error('JWT_KEY must be defined');
+        throw new Error('JWT_KEY must be defined!!!!');
     }
     if(!process.env.MONGO_URI){
         throw new Error('MONGO_URI must be defined');
