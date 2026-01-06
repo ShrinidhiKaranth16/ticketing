e15c7aeaeeecb8093d2f1e8f79c763b099fb6953
ShrinidhiKaranth16 <56083767+ShrinidhiKaranth16@users.noreply.github.com>
2026-01-06 15:46:21 +0530

    changed client

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -1,17 +1,18 @@
 import axios from 'axios';
 
-const buildClient = ({req}) => {
-    if(typeof window === 'undefined'){
-        return axios.create({
-            baseURL:'http://www.retix-app-prod.xyz',
-            headers:req.headers
-        })
-    }
-    else{
-        return axios.create({
-            baseURL:'/'
-        })
-    }
-}
+const buildClient = ({ req }) => {
+  if (typeof window === 'undefined') {
+    // 🚀 Server-side (inside Kubernetes)
+    return axios.create({
+      baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      headers: req.headers
+    });
+  } else {
+    // 🌍 Browser
+    return axios.create({
+      baseURL: '/'
+    });
+  }
+};
 
 export default buildClient;
